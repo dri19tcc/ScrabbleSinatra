793423662fdbd0d588532e0a4823422eb86e561b
Adriana <dri19tcc@gmail.com>
2016-04-08 10:40:57 -0700

    Added regex to form to allow only characters.  took away text that was unneccessary.  In score-many made default boxes show up on the page.

---
+++ lib/scrabble_sinatra.rb
@@ -0,0 +1,40 @@
+class Scoring
+  SCORE_CHART = {
+    "a" => 1, "e" => 1, "i" => 1, "o" => 1, "u" => 1, "l" => 1, "n" => 1, "r" => 1, "s" => 1, "t" => 1,
+    "d" => 2, "g" => 2,
+    "b" => 3, "c" => 3, "m" => 3, "p" => 3,
+    "f" => 4, "h" => 4, "v" => 4, "w" => 4, "y" => 4,
+    "k" => 5,
+    "j" => 8, "x" => 8,
+    "q" => 10, "z" => 10
+  }
+
+  def self.score(word)
+    # puts word
+    arr = word.downcase.split(//)
+    arr.length == 7 ? tally = 50 : tally = 0
+    arr.each do |i|
+      value = SCORE_CHART[i] || 0
+      tally = value + tally
+    end
+    return tally
+  end
+
+  def self.highest_score_from(array_of_words)
+    high_word = ""
+    high_score = 0
+    array_of_words.each do |word|
+      self.score(word)
+      if self.score(word) > high_score
+        high_word = word
+        high_score = self.score(word)
+      elsif self.score(word) == high_score
+        if word.length < high_word.length
+          high_word = word
+          high_score = self.score(word)
+        end
+      end
+    end
+    return high_word
+  end
+end
---
+++ scrabble.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require_relative 'lib/scrabble_sinatra'
+require 'pry'
+
+class Scrabble < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/score' do
+    erb :score
+  end
+
+  post '/score' do
+    @new_word = params["word"]
+    @score = Scoring.score(@new_word)
+    erb :score
+  end
+
+  get '/score-many' do
+    @number_words = 0
+    @words_with_score = {}
+    erb :score_many
+  end
+
+  post '/score-many' do
+    @number_words = params["number"].to_i #|| 0
+    @new_words = params["game"] #|| []
+    @words_with_score = {}
+    @new_words.each do |word|
+      @words_with_score[word] = Scoring.score(word.gsub(/[^a-zA-Z]/,""))
+    end
+    # return @words_with_score
+    erb :score_many
+  end
+
+  run!
+end
